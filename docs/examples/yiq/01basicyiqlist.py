dc3be2917767f95388199186615d930af6d5329b
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2020-07-14 18:39:02 +0200

    Add files via upload

--- docs/examples/yiq/01basicyiqlist.py
+++ docs/examples/yiq/01basicyiqlist.py
@@ -186,9 +186,9 @@ class TtkScale(Scale):
         self.digits = digits
         self.length = length
 
-        self.build(parent, from_, to, sliderlength, tickinterval)
+        self.build(parent, from_, to, sliderlength, tickinterval, length)
 
-    def build(self, parent, from_, to, sliderlength, tickinterval):
+    def build(self, parent, from_, to, sliderlength, tickinterval, length):
         """Create ticks
 
         Parameters
@@ -212,8 +212,8 @@ class TtkScale(Scale):
                 item = Label(parent, text=i, bg='#EFFEFF')
                 j = (i if from_ > 0 else i - from_)
                 item.place(in_=self, bordermode='outside',
-                           relx=sliderlength / sc_range / 2 +
-                           j / sc_range * (1 - sliderlength / sc_range),
+                           relx=sliderlength / length / 2 +
+                           j / sc_range * (1 - sliderlength / length),
                            rely=1, anchor='n')
 
 
